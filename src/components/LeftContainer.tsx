b842d81cf409cb8012ded5695dc6353c4d925483
Arsh <opsrc7094@gmail.com>
2024-02-11 15:08:14 +0530

    Dev:TailwindCSS setup

---
+++ src/components/LeftContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function LeftContainer(): React.JSX.Element { 
+    return (
+        <div className="bg-[#121212] p-4">
+        Left Container
+        </div>
+    )
+}
