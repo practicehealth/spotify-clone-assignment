ce34e09f0af9e87636035b1ddcbb7da9f14ff699
opsrc <opsrc7094@gmail.com>
2024-02-11 23:08:37 +0530

    Dev:data.json Adding mock data and favicon

--- src/components/Home/HomeNav.tsx
+++ src/components/Home/HomeNav.tsx
@@ -7,7 +7,7 @@ export default function HomeNav() {
                 <img className="h-10 w-10 bg-black cursor-pointer hover:bg-slate-800 p-2 rounded-full" src="assets/right.svg" alt="Next" />
             </div> 
             <div className="flex items-center">
-                <span className="text-slate-400 text-lg font-semibold ">Premium Support Download</span>
+                <span className="text-slate-400 text-lg font-semibold tracking-widest">Premium Support Download</span>
                 <div className="bg-gray-600 h-6 w-[2px] m-5"></div>
                 <button className="font-semibold text-slate-400 mr-5 hover:text-white">Signup</button>
                 <button className="bg-white text-black rounded-3xl font-semibold py-2 px-4 hover:scale-105">Login</button>
