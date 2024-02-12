9a8b10283209321378f2e5e5afa936354c73664d
Arsh <opsrc7094@gmail.com>
2024-02-12 10:54:28 +0530

    Dev:Setting up the CategoriesContainer

---
+++ src/components/Search/CategoriesContainer.tsx
@@ -0,0 +1,13 @@
+
+export default function CategoriesContainer() { 
+
+    return ( 
+        <div className="">
+            <div className="flex flex-wrap">
+                <span className="rounded-full bg-white ">
+                    All
+                </span>
+            </div>
+        </div>
+    )
+}
