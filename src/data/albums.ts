5533d576da059ec0cfaa98da85d69b58af4ad1e7
Sachin Gurjar <98685642+SachinCoder1@users.noreply.github.com>
2024-02-12 22:15:53 +0530

    react router dom, dummy data

---
+++ src/data/albums.ts
@@ -0,0 +1,59 @@
+import getLower from '../assets/1.jpg'
+import NeverBe from '../assets/2.jpg'
+import WeCantStop from '../assets/3.jpg'
+import Crush from '../assets/4.jpg'
+import Drop7 from '../assets/5.jpg'
+// import six from '../assets/6.jpg'
+// import seven from '../assets/7.jpg'
+// import eight from '../assets/8.jpg'
+// import nine from '../assets/9.jpg'
+// import ten from '../assets/10.jpg'
+
+
+export const albums = [
+    {
+      name: "Get Lower",
+      image: getLower,
+      type: "album",
+      external_urls: {
+        spotify: "https://open.spotify.com/album/6L3OPsn0P4F6PNUDFGxDVV",
+      },
+      artist_info: "Steve Aoki",
+    },
+    {
+      name: "Never Be Lonely",
+      image: NeverBe,
+      type: "album",
+      external_urls: {
+        spotify: "https://open.spotify.com/album/1DCiXnF1Z9XX4zxYCFytSX",
+      },
+      artist_info: "Jax Jones",
+    },
+    {
+      name: "We Can't Stop",
+      image: WeCantStop,
+      type: "album",
+      external_urls: {
+        spotify: "https://open.spotify.com/album/27LVMe5wMsHddairHoCux2",
+      },
+      artist_info: "AFROJACK",
+    },
+    {
+      name: "crush",
+      image: Crush,
+      type: "album",
+      external_urls: {
+        spotify: "https://open.spotify.com/album/0Tqz0PyiUMHr2HIwvXmyKP",
+      },
+      artist_info: "Alaina Castillo",
+    },
+    {
+      name: "Drop 7",
+      image: Drop7,
+      type: "album",
+      external_urls: {
+        spotify: "https://open.spotify.com/album/2jK2rbbnrzsId5WWSc67HP",
+      },
+      artist_info: "Little Simz",
+    },
+  ]
---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../components/sidebar/Sidebar'
+
+type Props = {}
+
+const Layout = (props: Props) => {
+  return (
+    <div>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default Layout
