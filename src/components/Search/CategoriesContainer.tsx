d823943d1cdc21a9c8500fa04e530c6218fde1dd
Arsh <opsrc7094@gmail.com>
2024-02-12 12:45:45 +0530

    Dev:CategoriesContainer

--- src/components/Search/CategoriesContainer.tsx
+++ src/components/Search/CategoriesContainer.tsx
@@ -1,3 +1,5 @@
+import dataJSON from "../../../data.json";
+
 const filters = ["All", "Songs", "Artists", "Playlists", "Albums", "Podcasts & Shows"]
 
 export default function CategoriesContainer() { 
@@ -14,7 +16,24 @@ export default function CategoriesContainer() {
                         ) 
                     })
                 }
-            </div> 
+            </div>
+            <div className="p-4">
+                <span className="text-xl text-white font-semibold">
+                    Browse All
+                </span>
+                <div className="flex flex-wrap my-3">
+                    {
+                        dataJSON['Categories'].map((data, idx)=>{ 
+                            return (
+                            <div key={idx} className={`bg-[#ff0000] w-[12rem] h-[12rem] flex flex-col px-4 py-3 overflow-hidden rounded-lg mr-5`}>
+                                <span className="font-semibold text-xl w-fit">{data.title}</span>
+                                <img className="relative bottom-0 p-4 right-0 h-30 w-30 rotate-[25deg] translate-x-12 translate-y-6" src="assets/sleep-1.jpeg" alt="Category" />
+                            </div>
+                            )
+                        })
+                    } 
+                </div>
+            </div>
         </div>
     )
 }
