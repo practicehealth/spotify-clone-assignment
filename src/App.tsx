58cdb331d78dd2c0dc6e43bae496aa92146590ce
shindebhavika <bhavikashinde74@gmail.com>
2024-02-17 17:06:56 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import Footer from "./components/Footer"
-import Home from "./components/Home"
-
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 function App() {
- 
-
- 
   return (
     <div className="w-screen h-screen tracking-wider overflow-x-hidden">
-<Home></Home>
-<Footer></Footer>
+      <Home></Home>
+      <Footer></Footer>
     </div>
- 
-  )
+  );
 }
-   
 
-
-export default App
+export default App;
